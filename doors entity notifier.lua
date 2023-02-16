915487b3d1a5f0d8bceaf2fdc25bdd11c39c482e
whoisitlolxd <95188941+whoisitlolxd@users.noreply.github.com>
2023-02-16 01:23:45 -0400

    Update doors entity notifier.lua

--- doors entity notifier.lua
+++ doors entity notifier.lua
@@ -1,6 +1,6 @@
 getgenv = getgenv or getgenv == nil and (function()error('getgenv doesnt exist get a better executor')end)()
 
-local ws = game:GetService('Workspace')
+--[[local ws = game:GetService('Workspace')
 
 ws.Ambience:ClearAllChildren() -- no annoying ambience
 
@@ -74,4 +74,101 @@ ws.ChildAdded:Connect(function(v)
     end)
 end)
 
-notif('Placeholder title','Script is running. Ignore this.',3)
+notif('Placeholder title','Script is running. Ignore this.',3)]]
+
+-- updated
+
+local ws = game:GetService('Workspace')
+
+function bbgui(v)
+    local gui = Instance.new('BillboardGui')
+    local imglabel = Instance.new('ImageLabel')
+    imglabel.Size = UDim2.fromScale(1,1)
+    gui.Size = UDim2.fromScale(5,5)
+    imglabel.BackgroundTransparency = 1
+    imglabel.Image = 'rbxassetid://6525485108'
+    imglabel.Parent = gui
+    gui.LightInfluence = 0
+    gui.AlwaysOnTop = true
+    gui.Parent = v
+end
+
+ws.Ambience:ClearAllChildren()
+
+ws.Terrain.DescendantAdded:Connect(function(v)
+    if v:IsA'Sound' then
+	    v:Destroy()
+    end
+end)
+
+ws.Ambience.DescendantAdded:Connect(function(v)
+    if v:IsA'Sound' then
+	    v:Destroy()
+    end
+end)
+
+local run = game['Run Service']
+
+function notif(title,text,dur,icon)
+    game.StarterGui:SetCore('SendNotification',{
+	Icon = icon == true and 'rbxassetid://6525485108' or nil;
+        Title = title;
+        Text = text;
+        Duration = tonumber(dur) or 3;
+        }
+    )
+end
+
+getgenv().notif = notif
+
+-- these are necessary
+getgenv().functions = {}
+getgenv().functions.hb = {}
+getgenv().functions.hb.nowindows = function()
+    for i,v in pairs(ws.CurrentRooms:GetChildren()) do
+        for b,n in pairs(v:GetDescendants()) do if n:IsA('Sound') then v.PlaybackSpeed = 0; v.Volume = 0; v:Stop(); end end -- windows make noise. get rekt
+    end
+end
+
+-- screech alert
+getgenv().functions.screech = function(v)
+    if v.Name == 'Screech' then
+        notif('Entity Warning!','Screech has spawned, look around!',3)
+        bbgui(v:FindFirstChildWhichIsA('Part',true) or v:FindFirstChildWhichIsA('BasePart',true) or v:FindFirstChildWhichIsA('MeshPart',true))
+    end
+end
+
+-- screech alert
+ws.Camera.ChildAdded:Connect(getgenv().functions.screech)
+
+run.Heartbeat:Connect(function()
+    for i,func in pairs(getgenv().functions.hb) do
+        func()
+    end
+end)
+
+-- monster detector
+ws.ChildAdded:Connect(function(v)
+    if not ((v.Name == 'RushMoving' or v.Name == 'AmbushMoving' or v.Name == 'SeekMoving') and v:IsA('Model')) then
+        return
+    end
+    local loop = nil
+    loop = game['Run Service'].Stepped:Connect(function()
+        if (v:FindFirstChildOfClass('BasePart') or v.PrimaryPart) ~= nil then
+            local homepart = v:FindFirstChildOfClass('BasePart') or v.PrimaryPart
+            local mag = game.Players.LocalPlayer:DistanceFromCharacter(homepart.Position)
+            if mag < 10000 then
+                bbgui(homepart)
+                getgenv().notif('Entity Warning!','\"'..v.Name..'\" has spawned!',3)
+                loop:Disconnect()
+                loop = nil
+            end
+        end
+        if v == nil then
+            loop:Disconnect()
+            loop = nil
+        end
+    end)
+end)
+
+--notif('Placeholder title','Script is running. Ignore this.',3)
