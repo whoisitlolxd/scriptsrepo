f4f350579735f7c8a964ab7a414ed4f136645ab4
whoisitlolxd <95188941+whoisitlolxd@users.noreply.github.com>
2023-02-16 02:05:10 -0400

    small fix

--- sprinter.lua
+++ sprinter.lua
@@ -1,4 +1,5 @@
-function sprinter(args:{} --[[keyyy:Enum.KeyCode,sped:number,barmin:number,barmax:number,sart:number,dispaymax:number]] )
+function sprinter(arg:{} --[[keyyy:Enum.KeyCode,sped:number,barmin:number,barmax:number,sart:number,dispaymax:number]] )
+	local args = arg or {}
 	local speed = math.abs(tonumber(args.speed) or 5) --[[or math.abs(args[2])]] or 5
 	local keyy = args.key --[[or args[1] ]] or Enum.KeyCode.Z
 	local barminn = args.barmin --[[or args[3] ]] or -5
